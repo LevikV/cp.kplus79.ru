433fdcfdf1afb21c4a8f11b9f16f2cbe904ffec9
LevikV <alex@kplus79.ru>
2021-04-29 16:08:22 +1000

    работаю с выгрузкой 4

--- public_html/obrabotka.php
+++ public_html/obrabotka.php
@@ -1,9 +1,12 @@
 <?php
 $kurs = 77;
+//
 $artikulOld = array();
 $fOld = fopen('vtt_price_im.csv', 'r');
-while ($data = fgetcsv($fOld, 1000, ';', '"') != FALSE) {
-    $artikulOld[] = $data[0];
+$i = 0;
+while (($data = fgetcsv($fOld, 0, ';', '"')) !== FALSE) {
+    if ($i == 1) $artikulOld[] = $data[0];
+    $i = 1;
 }
 fclose($fOld);
 //
@@ -14,25 +17,30 @@ $fNew = fopen('new_goods.txt', 'w');
 $caption = array("Artikul", "Name", "Manafacture", "Description", "Group0", "Group1", "Group2", "Quantity", "Price", "Width",
     "Height", "Depth", "Weight", "PhotoUrl", "PartNumber", "Vendor", "Compatibility", "ColorName");
 fputcsv($fIm, $caption, ';', '"');
-while ($data = fgetcsv($fVtt, 1000, ';', '"') != FALSE) {
-    //Накрутка цены, в зависимости оригинал или нет
-    if ((stripos($data[1], '(o)')===false) AND (stripos($data[1], '( o )')===false) AND (stripos($data[1], '(0)')===false) AND (stripos($data[1], '( 0 )')===false)) {
-        $price = $data[8];
-        $price = $price*$kurs+$price*$kurs*0.4;
-        if ($price === 0) $price = '';
-        $data[8] = $price;
-    } else {
-        $price = $data[8];
-        $price = $price*$kurs+$price*$kurs*0.1;
-        if ($price === 0) $price = '';
-        $data[8] = $price;
+$i = 0;
+while (($data = fgetcsv($fVtt, 1000, ';', '"')) !== FALSE) {
+    if ($i==1) {
+        //Накрутка цены, в зависимости оригинал или нет
+        if ((stripos($data[1], '(o)')===false) AND (stripos($data[1], '( o )')===false) AND (stripos($data[1], '(О)')===false) AND (stripos($data[1], '( о )')===false)) {
+            $price = $data[8];
+            $price = $price*$kurs+$price*$kurs*0.4;
+            if ($price === 0) $price = '';
+            $data[8] = $price;
+        } else {
+            $price = $data[8];
+            $price = $price*$kurs+$price*$kurs*0.1;
+            if ($price === 0) $price = '';
+            $data[8] = $price;
+            echo $data[0] . "  " . $data[1] . "<br>";
+        }
+        //Записываем прайс лист для ИМ
+        fputcsv($fIm, $data, ';', '"');
+        //Проверяем новый это товар или уже есть в базе, по артиклу
+        if (!in_array ($data[0], $artikulOld)) {
+            fputs($fNew, $data[0]."\r\n");
+        }
     }
-    //Записываем прайс лист для ИМ
-    fputcsv($fIm, $data, ';', '"');
-    //Проверяем новый это товар или уже есть в базе, по артиклу
-    if (!in_array ($data[0], $artikulOld)) {
-        fputs($fNew, $data[0]."\r\n");
-    }
+    $i = 1;
 }
 fclose($fIm);
 fclose($fVtt);
