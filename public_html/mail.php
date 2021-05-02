a64bd902c665106c4a054dca0c01139f59e6e3f3
LevikV <alex@kplus79.ru>
2021-05-02 15:26:51 +1000

    работаю с выгрузкой 9

--- public_html/mail.php
+++ public_html/mail.php
@@ -1,10 +1,11 @@
 <?php
 //mail("alex@kplus79.ru", "Test from my php", "This is body of mail");
-$to      = 'alex@kplus79.ru';
+$to      = "<alex@kplus79.ru>, ";
+$to      .= "<info@kplus79.ru>";
 $subject = 'the subject';
-$message = 'hello\r\ntest\r\nwow';
+$message = "hello\r\n";
+$message .= "test\r\n";
 $headers = 'From: noreply@kplus79.ru' . "\r\n" .
-    'Reply-To: info@kplus79.ru' . "\r\n" .
     'X-Mailer: PHP/' . phpversion();
 
 mail($to, $subject, $message, $headers);
