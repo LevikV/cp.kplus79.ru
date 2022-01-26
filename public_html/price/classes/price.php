56643e6fe20379c8125ae35f2ba1eb210a4b61ba
LevikV <alex@kplus79.ru>
2022-01-27 09:54:23 +1000

    Работаю над обновлением/добавлением товаров в прайс лист

--- public_html/price/classes/price.php
+++ public_html/price/classes/price.php
@@ -10,28 +10,71 @@ class Price {
         } else {
             // Ошибка получения списка продуктов из нашей базы, прерываем работу скрипта
         }
+
         // Получаем список всех поставщиков
         $providers = $db->getProviders();
+
         // Получаем карту сопоставлений по товарам
         $maps = $db->getMaps('product');
         //Собираем id продуктов поставщиков сопоставленных с нашей эталонной базой
-        $maps_id = array();
+        $map_products_id = array();
         if ($maps !== false) {
             if ($maps !== null) {
                 foreach ($maps as $map) {
-                    $maps_id[] = $map['provider_id'];
+                    $map_products_id[] = $map['provider_id'];
+                }
+            }
+        }
+
+        // Получаем карту сопоставлений по моделям
+        $maps = $db->getMaps('model');
+        //Собираем id моделей поставщиков сопоставленных с нашей эталонной базой
+        $map_models_id = array();
+        if ($maps !== false) {
+            if ($maps !== null) {
+                foreach ($maps as $map) {
+                    $map_models_id[] = $map['provider_id'];
+                }
+            }
+        }
+
+        // Получаем карту сопоставлений по производителям
+        $maps = $db->getMaps('manufacturer');
+        //Собираем id производителей поставщиков сопоставленных с нашей эталонной базой
+        $map_manufs_id = array();
+        if ($maps !== false) {
+            if ($maps !== null) {
+                foreach ($maps as $map) {
+                    $map_manufs_id[] = $map['provider_id'];
+                }
+            }
+        }
+
+        // Получаем карту сопоставлений по вендорам
+        $maps = $db->getMaps('vendor');
+        //Собираем id вендоров поставщиков сопоставленных с нашей эталонной базой
+        $map_vendors_id = array();
+        if ($maps !== false) {
+            if ($maps !== null) {
+                foreach ($maps as $map) {
+                    $map_vendors_id[] = $map['provider_id'];
                 }
             }
         }
 
         foreach ($providers as $provider) {
             if ($provider['parent_id'] == null) {
+                // Перед загрузкой товаров необходимо проверить, сопоставлены ли основные данные
+                // проверяем сопоставлены ли модели по текущему поставщику
+
+
+
                 // Получаем все товары текущего поставщика
                 $provider_products = array();
                 $provider_products = $db->getProviderProducts($provider['id']);
                 // Для каждого товара поставщика проверяем, есть ли он в таблице сопоставлений
                 foreach ($provider_products as $provider_product) {
-                    if (!in_array($provider_product['id'], $maps_id, true)) {
+                    if (!in_array($provider_product['id'], $map_products_id, true)) {
                         // если товар из таблицы поставщиков отсутствует в нашем прайсе, то его нужно сопоставить
                         // Проходим по каждому продукту из нашей базы и пытаемся найти соответствие
                         foreach ($products as $product) {
