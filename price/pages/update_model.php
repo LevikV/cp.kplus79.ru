6603574c82801f4f8636d69304bd48de72224d66
LevikV <alex@kplus79.ru>
2022-02-01 20:01:03 +1000

    Работаю над интерфейсом

--- price/pages/update_model.php
+++ price/pages/update_model.php
@@ -6,7 +6,7 @@
                 <li class="breadcrumb-item"><a href="index.php?route=price&code=main">Модуль управления Прайс листом</a></li>
                 <li class="breadcrumb-item active">Обновление моделей</li>
             </ol>
-            <div>
+            <div class="col">
                 <p class="mb-0">Список добавленных карт сопоставления</p>
                 <table class="table">
                     <thead>
@@ -23,11 +23,49 @@
                     <tbody>
                     <?
                     if (!empty($data['models_map_adds'])) {
-                        echo '<tr>';
+                        $i = 1;
                         foreach ($data['models_map_adds'] as $model_map) {
-
+                            echo '<tr>';
+                            echo '<td>' .$i . '</td>';
+                            echo '<td>' .$model_map[''] . '</td>';
+                            echo '<td>' .$model_map[''] . '</td>';
+                            echo '<td>' .$model_map[''] . '</td>';
+                            echo '<td>' .$model_map[''] . '</td>';
+                            echo '<td>' .$model_map[''] . '</td>';
+                            echo '</tr>';
+                            $i++;
                         }
-                        echo '</tr>';
+                    }
+                    ?>
+                    </tbody>
+                </table>
+            </div>
+            <div class="col">
+                <p class="mb-0">Список моделей для добавления в эталонную базу</p>
+                <table class="table">
+                    <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>id поставщика</th>
+                        <th>Имя поставщика</th>
+                        <th>id модели поставщика</th>
+                        <th>Имя модели поставщика</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <?
+                    if (!empty($data['models_to_add'])) {
+                        $i = 1;
+                        foreach ($data['models_to_add'] as $model) {
+                            echo '<tr>';
+                            echo '<td>' .$i . '</td>';
+                            echo '<td>' .$model['provider_id'] . '</td>';
+                            echo '<td>' .$model['provider_name'] . '</td>';
+                            echo '<td>' .$model['prov_model_id'] . '</td>';
+                            echo '<td>' .$model['prov_model_name'] . '</td>';
+                            echo '</tr>';
+                            $i++;
+                        }
                     }
                     ?>
                     </tbody>
