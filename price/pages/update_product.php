2efdbff0b28ef8a06e5ed1db4e31b0f560e125e2
LevikV <alex@kplus79.ru>
2022-02-22 18:57:31 +1000

    Начал делать обновление товаров

--- price/pages/update_product.php
+++ price/pages/update_product.php
@@ -32,17 +32,17 @@
                     </thead>
                     <tbody>
                     <?
-                    if (!empty($data['attrib_values_map_adds'])) {
+                    if (!empty($data['products_map_adds'])) {
                         $i = 1;
-                        foreach ($data['attrib_values_map_adds'] as $attrib_value_map) {
+                        foreach ($data['products_map_adds'] as $product_map) {
                             echo '<tr>';
                             echo '<td>' .$i . '</td>';
-                            echo '<td>' .$attrib_value_map['id'] . '</td>';
-                            echo '<td>' .$attrib_value_map['attrib_value'] . '</td>';
-                            echo '<td>' .$attrib_value_map['attrib_value_id'] . '</td>';
-                            echo '<td>' .$attrib_value_map['prov_attrib_value'] . '</td>';
-                            echo '<td>' .$attrib_value_map['prov_attrib_value_id'] . '</td>';
-                            echo '<td>' .$attrib_value_map['provider_name'] . '</td>';
+                            echo '<td>' .$product_map['id'] . '</td>';
+                            echo '<td>' .$product_map['product_name'] . '</td>';
+                            echo '<td>' .$product_map['product_id'] . '</td>';
+                            echo '<td>' .$product_map['prov_product_name'] . '</td>';
+                            echo '<td>' .$product_map['prov_product_id'] . '</td>';
+                            echo '<td>' .$product_map['provider_name'] . '</td>';
                             echo '</tr>';
                             $i++;
                         }
@@ -52,36 +52,52 @@
                 </table>
             </div>
             <div class="col">
-                <p class="mb-0">Список значений аттрибутов для добавления в эталонную базу</p>
-                <table id="tableAttribValues" class="table table-sm">
+                <p class="mb-0">Список товаров для добавления в эталонную базу</p>
+                <table id="tableProducts" class="table table-sm">
                     <thead>
                     <tr>
                         <th rowspan="2">#</th>
-                        <th>id эталонного аттрибута</th>
-                        <th>Имя эталонного аттрибута</th>
-                        <th rowspan="2">id значения аттрибута поставщика</th>
-                        <th rowspan="2">Значение аттрибута поставщика</th>
-                        <th rowspan="2">id поставщика</th>
-                        <th rowspan="2">Имя поставщика</th>
-                        <th rowspan="2"></th>
-                        <th rowspan="2"></th>
+                        <th rowspan="2">id товара</th>
+                        <th rowspan="2">Наименование</th>
+                        <th rowspan="2">Описание</th>
+                        <th>id категории поставщика</th>
+                        <th>id модели поставщика</th>
+                        <th>id вендора поставщика</th>
+                        <th>id производит. поставщика</th>
+                        <th rowspan="2">Аттрибуты</th>
+                        <th rowspan="2">Ширина</th>
+                        <th rowspan="2">Высота</th>
+                        <th rowspan="2">Длина</th>
+                        <th rowspan="2">Вес</th>
+                        <th rowspan="2">Версия</th>
+                        <th rowspan="2">Изображения</th>
+                        <th rowspan="2">Статус</th>
+                        <th rowspan="2">Дата добавления</th>
+                        <th rowspan="2">Дата редактирования</th>
+                        <th rowspan="2">Дата обновления</th>
                     </tr>
                     <tr>
-                        <th>id аттриб поставщика</th>
-                        <th>Имя аттриб поставщика</th>
+                        <th>Имя категории</th>
+                        <th>Имя модели</th>
+                        <th>Имя вендора</th>
+                        <th>Имя производит.</th>
                     </tr>
                     </thead>
                     <tbody>
                     <?
-                    if (!empty($data['attrib_values_to_add'])) {
+                    if (!empty($data['products_to_add'])) {
                         $i = 1;
-                        foreach ($data['attrib_values_to_add'] as $attrib_value_to_add) {
+                        foreach ($data['products_to_add'] as $product_to_add) {
                             echo '<tr id="' . $i . '">';
                             echo '<td rowspan="2">' .$i . '</td>';
-                            echo '<td class="attrib-id">' .$attrib_value_to_add['attrib_id'] . '</td>';
-                            echo '<td class="attrib-name">' .$attrib_value_to_add['attrib_name'] . '</td>';
-                            echo '<td class="prov-attrib-value-id" rowspan="2">' .$attrib_value_to_add['prov_attrib_value_id'] . '</td>';
-                            echo '<td class="prov-attrib-value" rowspan="2">' .$attrib_value_to_add['prov_attrib_value'] . '</td>';
+                            echo '<td rowspan="2" class="prov-product-id">' .$product_to_add['prov_product_id'] . '</td>';
+                            echo '<td rowspan="2" class="prov-product-name">' .$product_to_add['prov_product_name'] . '</td>';
+                            echo '<td rowspan="2" class="prov-product-description">' .$product_to_add['prov_product_description'] . '</td>';
+                            echo '<td class="prov-product-category-id">' .$product_to_add['prov_product_category_id'] . '</td>';
+                            echo '<td class="prov-product-model-id">' .$product_to_add['prov_product_model_id'] . '</td>';
+                            echo '<td class="prov-product-vendor-id">' .$product_to_add['prov_product_vendor_id'] . '</td>';
+                            echo '<td class="prov-product-manuf-id">' .$product_to_add['prov_product_manuf_id'] . '</td>';
+
                             echo '<td class="prov-id" rowspan="2">' .$attrib_value_to_add['provider_id'] . '</td>';
                             echo '<td class="prov-name" rowspan="2">' .$attrib_value_to_add['provider_name'] . '</td>';
                             echo '<td rowspan="2">Сопоставить</td>';
@@ -99,8 +115,9 @@
                             echo '</tr>';
                             //
                             echo '<tr id="' . $i . '-child">';
-                            echo '<td class="prov-attrib-id">' .$attrib_value_to_add['prov_attrib_id'] . '</td>';
-                            echo '<td class="prov-attrib-name">' .$attrib_value_to_add['prov_attrib_name'] . '</td>';
+                            echo '<td class="prov-product-category-name">' .$product_to_add['prov_product_category_name'] . '</td>';
+                            echo '<td class="prov-product-model-name">' .$product_to_add['prov_product_model_name'] . '</td>';
+                            echo '<td class="prov-product-vendor-name">' .$product_to_add['prov_product_vendor_name'] . '</td>';
                             echo '</tr>';
 
                             $i++;
