38523fc0c571c7cf5a22ed2a3b2221a7225b6009
LevikV <alex@kplus79.ru>
2022-03-27 16:42:10 +1000

    надо разбираться с путями виндовс и линукс

--- price/modules/update/update_price_runtime.php
+++ price/modules/update/update_price_runtime.php
@@ -7,10 +7,12 @@
 ignore_user_abort(true);
 // Включаем автоподгрузку классов
 spl_autoload_register(function ($class) {
-    include $_SERVER['DOCUMENT_ROOT'] . '/price/classes/' . $class . '.php';
+    //include $_SERVER['DOCUMENT_ROOT'] . '/price/classes/' . $class . '.php';
+    include 'price\classes\\' . $class . '.php';
 });
 // Загружаем глобальные настройки
-require_once($_SERVER['DOCUMENT_ROOT'] . '/price/system/config.php');
+//require_once($_SERVER['DOCUMENT_ROOT'] . '/price/system/config.php');
+require_once('price\system\config.php');
 // Объявляем глобальный массив ошибок
 $ERROR = array();
 
